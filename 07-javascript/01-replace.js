f88db2a54eab298f5da11583a4b7cf2941a12207
Niranjan Varma <nvarma@pdx.edu>
2025-11-07 20:05:10 -0800

    till part 3 complete

--- 07-javascript/01-replace.js
+++ 07-javascript/01-replace.js
@@ -4,6 +4,7 @@ const replaceItems = (input) => {
   // for reference, see the MDN "Regular Expressions" entry:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+  input = input.replace(/gold/gi, "SHINY").replace(/wander/i, "roam");
   return input;
 };
 
@@ -13,4 +14,3 @@ console.log(replaceItems("Wisdom is better than silver or GOLD."));
 // expected: 'Wisdom is better than silver or SHINY.'
 console.log(replaceItems("Not all those who Wander are lost"));
 // expected: 'Not all those who roam are lost'
-yo;
